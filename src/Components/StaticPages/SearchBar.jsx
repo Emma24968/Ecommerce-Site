f4f52eface1af23df45963527564dfdb3e7a874b
Emma24968 <emnabs12@gmail.com>
2025-12-12 00:56:12 +0100

    I added the user icons

--- src/Components/StaticPages/SearchBar.jsx
+++ src/Components/StaticPages/SearchBar.jsx
@@ -1,10 +1,16 @@
+import { Bell, CircleUserRound, MessageSquare } from "lucide-react"
+
 export const SearchBar = () =>{
     return(
         <div>
             <div className="search-box">
                 <input type="text" placeholder="search something" />
             </div>
-            <div className="User"></div>
+            <div className="User">
+                <p><Bell /></p>
+                <p><MessageSquare /></p>
+                <p><CircleUserRound /></p>
+            </div>
         </div>
     )
 }
