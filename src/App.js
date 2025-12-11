a49a2bd52b7ee040c1b5aaf740907a44954b5c09
Emma24968 <emnabs12@gmail.com>
2025-12-12 00:51:30 +0100

    I added the search box

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import './App.css';
-import { SideBar } from './Components/StaticPages/LeftSideBar';
+import { Search } from "lucide-react";
+import "./App.css";
+import { SideBar } from "./Components/StaticPages/LeftSideBar";
+import { SearchBar } from "./Components/StaticPages/SearchBar";
 
 function App() {
   return (
-    <>
-    <SideBar />
-    </>
+    <div>
+      <div>
+        <SideBar />
+        <SearchBar />
+      </div>
+    </div>
   );
 }
 
--- src/Components/StaticPages/SearchBar.jsx
+++ src/Components/StaticPages/SearchBar.jsx
@@ -1,7 +1,10 @@
 export const SearchBar = () =>{
     return(
         <div>
-            
+            <div className="search-box">
+                <input type="text" placeholder="search something" />
+            </div>
+            <div className="User"></div>
         </div>
     )
 }
