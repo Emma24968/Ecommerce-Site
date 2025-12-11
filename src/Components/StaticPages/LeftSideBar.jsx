3075834e48b598f8854e9048e6fe0305a1b19596
Emma24968 <emnabs12@gmail.com>
2025-12-12 00:42:25 +0100

    I added all my sidebar icons

--- src/Components/StaticPages/LeftSideBar.jsx
+++ src/Components/StaticPages/LeftSideBar.jsx
@@ -1,10 +1,28 @@
-import { Codesandbox } from "lucide-react"
+import { Codesandbox, TrendingUp, MessageSquare, Users, Settings, Info, LogOut } from "lucide-react";
 
-export const SideBar = () =>{
-    return(
-        <div>
-           <h1>Logo</h1> 
-           <p><Codesandbox /></p>
-        </div>
-    )
-}
+export const SideBar = () => {
+  return (
+    <div>
+      <h1>Logo</h1>
+      <p>
+        <Codesandbox />
+      </p>
+      <p>
+        <TrendingUp />
+      </p>
+      <p>
+        <MessageSquare />
+      </p>
+      <p>
+        <Users />
+      </p>
+      <p>
+        <Settings />
+      </p>
+      <p>
+        <Info />
+      </p>
+      <p><LogOut /></p>
+    </div>
+  );
+};
