4d65d2e2f0f5da5f7a87d1395efa3bab3882011a
Emma24968 <emnabs12@gmail.com>
2025-12-12 03:05:08 +0100

    I added the search container and stlyled it

--- src/Components/StaticPages/Navigation/SearchBar.jsx
+++ src/Components/StaticPages/Navigation/SearchBar.jsx
@@ -1,17 +1,27 @@
-import { Bell, CircleUserRound, MessageSquare } from "lucide-react"
+import { Bell, CircleUserRound, MessageSquare, Search } from "lucide-react";
 
-export const SearchBar = (props) =>{
-    return(
-        // I added a classname using the spread method
-        <div{...props}>
-            <div className="search-box">
-                <input type="text" placeholder="search something" />
-            </div>
-            <div className="User">
-                <p><Bell /></p>
-                <p><MessageSquare /></p>
-                <p><CircleUserRound /></p>
-            </div>
+export const SearchBar = (props) => {
+  return (
+    // I added a classname using the spread method
+    <div {...props}>
+      <div className="search-box">
+        <div className="search-container">
+          <input type="text" placeholder="search something" />
+          <p id="line">|</p>
+          <Search />
         </div>
-    )
-}
+      </div>
+      <div className="user">
+        <p>
+          <Bell />
+        </p>
+        <p>
+          <MessageSquare />
+        </p>
+        <p>
+          <CircleUserRound />
+        </p>
+      </div>
+    </div>
+  );
+};
