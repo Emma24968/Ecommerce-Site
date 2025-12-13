5239829453ee89d94df59c3bc070dc4e2bff0b7b
Emma24968 <emnabs12@gmail.com>
2025-12-13 05:08:57 +0100

    I started styling my price container

--- src/Components/StaticPages/Filter/Range.jsx
+++ src/Components/StaticPages/Filter/Range.jsx
@@ -1,17 +1,23 @@
 import { useState } from "react";
 import RangeSlider from "react-range-slider-input";
 import "react-range-slider-input/dist/style.css";
+import './Range.css'
 
 export const PriceRange = () => {
   const [price, setPrice] = useState([0, 1000]);
   return (
     <div>
       <h3>Price Range</h3>
-      <div>
+      <div className="price-container">
         <span>${price[0]}</span>
         <span>${price[1]}</span>
-        <RangeSlider />
       </div>
+        <RangeSlider min={0}
+        max={1000}
+        step={10}
+        value={price}
+        onInput={setPrice}
+/>
     </div>
   );
 };
