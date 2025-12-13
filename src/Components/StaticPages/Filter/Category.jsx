281e95d972fb30955ca323171af08ee86a096534
Emma24968 <emnabs12@gmail.com>
2025-12-13 03:19:52 +0100

    I created the list container

--- src/Components/StaticPages/Filter/Category.jsx
+++ src/Components/StaticPages/Filter/Category.jsx
@@ -1,6 +1,22 @@
-export const Category =()=>{
-    return(
-        <div>
+import "./Category.css";
+export const Category = () => {
+  return (
+    <div>
+      <h3>Category</h3>
+      <div className="list-container">
+        <div className="list">
+          <button>All Items</button>
+          <button>On Sale</button>
         </div>
-    )
-}
+        <div className="list">
+          <button>Featured</button>
+          <button>Best Seller</button>
+        </div>
+        <div className="list">
+          <button>New Items</button>
+          <button>On Sale</button>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/Components/StaticPages/Filter/Filter.jsx
+++ src/Components/StaticPages/Filter/Filter.jsx
@@ -1,10 +1,12 @@
+import { Category } from './Category'
 import './Filter.css'
 import { Header } from "./Header"
 
 export const Filter = ()=>{
     return(
         <div>
-          <Header />  
+          <Header /> 
+          <Category /> 
         </div>
     )
 }
