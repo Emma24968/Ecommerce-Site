efbdaf512e88b5339d41fd8cca7506f9ff702f3c
Emma24968 <emnabs12@gmail.com>
2025-12-13 05:54:28 +0100

    I created and styled the rating component

--- src/Components/StaticPages/Filter/Rating.jsx
+++ src/Components/StaticPages/Filter/Rating.jsx
@@ -1,15 +1,38 @@
-import { Circle, CircleCheckBig, Star } from "lucide-react"
+import { Circle, CircleCheckBig, Star } from "lucide-react";
+import "./Rating.css";
 
-export const Rating =()=>{
-    return(
-        <div>
-            <h3>Rating</h3>
-            <div className="rating">
-<div>
-    <CircleCheckBig /> <span>5</span><Star />
-   <Circle /> <span>4</span><Star />
-</div>
-            </div>
+export const Rating = () => {
+  return (
+    <div>
+      <h3>Rating</h3>
+      <div className="rating-container">
+        <div className="rating">
+          <div className="rating-item">
+            {" "}
+            <CircleCheckBig /> <span>5</span>
+            <Star />
+          </div>
+          <div className="rating-item">
+            {" "}
+            <Circle /> <span>4</span>
+            <Star />
+          </div>
+          <div className="rating-item">
+            <Circle /> <span>3</span>
+            <Star />
+          </div>
         </div>
-    )
-}
+        <div className="rating">
+          <div className="rating-item">
+            <Circle /> <span>2</span>
+            <Star />
+          </div>
+          <div className="rating-item">
+            <Circle /> <span>1</span>
+            <Star className="gold-star"/>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
