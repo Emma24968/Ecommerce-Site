def33f0850fa7041db23de0b9a57872357f47c13
Emma24968 <emnabs12@gmail.com>
2025-12-13 06:14:20 +0100

    I added horizontal line to demarcate various component in my parent file

--- src/Components/StaticPages/Filter/Filter.jsx
+++ src/Components/StaticPages/Filter/Filter.jsx
@@ -1,18 +1,25 @@
-import { Category } from './Category'
-import './Filter.css'
-import { Header } from "./Header"
-import Range, { PriceRange } from './Range'
-import { Rating } from './Rating'
-import { Size } from './Size'
+import { Category } from "./Category";
+import "./Filter.css";
+import { Header } from "./Header";
+import Range, { PriceRange } from "./Range";
+import { Rating } from "./Rating";
+import { Size } from "./Size";
 
-export const Filter = ()=>{
-    return(
-        <div>
-          <Header /> 
-          <Category /> 
-          <Size />
-          <PriceRange />
-          <Rating />
-        </div>
-    )
-}
+export const Filter = () => {
+  return (
+    <div>
+      <Header />
+      <hr />
+      <Category />
+      <hr />
+
+      <Size />
+      <hr />
+
+      <PriceRange />
+      <hr />
+
+      <Rating />
+    </div>
+  );
+};
