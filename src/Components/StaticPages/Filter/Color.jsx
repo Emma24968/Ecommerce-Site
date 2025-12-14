de60f9c905d089bb9202cefd0193b775ef64a85e
Emma24968 <emnabs12@gmail.com>
2025-12-14 07:20:17 +0100

    I created my color filter component

---
+++ src/Components/StaticPages/Filter/Color.jsx
@@ -0,0 +1,12 @@
+import './Color.css'
+export const Color = ()=>{
+return(<div class="color-filter">
+  <div class="color-circle clear">×</div>
+  <div class="color-circle" style="background-color: yellow;"></div>
+  <div class="color-circle" style="background-color: red;"></div>
+  <div class="color-circle" style="background-color: blue;"></div>
+  <div class="color-circle" style="background-color: green;"></div>
+  <div class="color-circle" style="background-color: black;"></div>
+</div>
+)
+}
